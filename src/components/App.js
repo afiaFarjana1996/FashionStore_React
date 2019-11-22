806d72302826c2afeeae73c5be74fa21a4ef75f1
JieningLiSS <jiening.li@smoothstack.com>
2019-11-22 00:57:43 -0500

    log in and register

--- src/components/App.js
+++ src/components/App.js
@@ -70,6 +70,7 @@ const App = () => (
             <Route path='/pants'
                 render={() => (<ProductList category='Pants'
                     products={pants}/>)} />
+            
         </Switch>
     </div>
 )
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,19 @@ export const Header = () => (
             <ul className="list-inline">
                 <li className="list-inline-item">
                     <Link to="/" className="navbar-brand">
-                        <img width="90px" height="30px" src="images/logo.png" />
+                        <img width="170px" height="140px" src="images/logo.png" />
                     </Link>
                 </li>
                 <li className="list-inline-item"><Link to="/shoes" replace>Shoes</Link></li>
                 <li className="list-inline-item"><Link to="/shirts" replace>Shirts</Link></li>
                 <li className="list-inline-item"><Link to="/pants" replace>Pants</Link></li>
             </ul>
+            
+            <ul className="list-inline">
+            <li className="list-inline-item"><Link to="/login" replace>Log in</Link></li>
+            <li className="list-inline-item"><Link to="/login" replace>Register</Link></li>
+            </ul>
+          
         </div>
     </nav>
 )
